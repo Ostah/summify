741e1d66d9fbdd927a076e1002f70c136bee2c54
Ostah <ostah@outlook.com>
2018-04-19 21:44:29 +0200

    Initial commit

---
+++ src/main/java/com/ostah/summondo/summondo/SummondoApplication.java
@@ -0,0 +1,12 @@
+package com.ostah.summondo.summondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummondoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ostah/summondo/summondo/SummondoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ostah.summondo.summondo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SummondoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
